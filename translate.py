a4fdd4d87130db9094c9ed4ba0aaba77579ad6ac
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-27 20:58:51 -0700

    Last attemp Q6 It needs some changes yet.

--- translate.py
+++ translate.py
@@ -197,21 +197,19 @@ def get_longest_peptide(rna_sequence, genetic_code):
     rna_seq_new = rna_seq_new.strip()
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
 
-    longpep = get_all_translations(rna_sequence = rna_seq_new[i:], genetic_code = genetic_code)
-    rev_comp = reverse_and_complement(sequence = rna_seq_new[i:])
+    longpep = get_all_translations(rna_sequence = rna_seq_new, genetic_code = genetic_code)
+    rev_comp = reverse_and_complement(sequence = rna_seq_new)
     rev_comp_longpep = get_all_translations(rna_sequence = rev_comp, genetic_code = genetic_code)
 
     longpep = rev_comp_longpep + longpep
-
-#    newleng = -1
+    longestpeptide = str(i)
 
     for i in longpep:
         newleng = -1
-        if len(longpep) < 1:
-            return ''
-        if len(i) > newleng:
+        if len(longpep) < 0:
+            return ""
+        elif len(i) > newleng:
             newleng = len(i)
-            longestpeptide = str(i)
 
     return longestpeptide
 
